d591a598ecff1569a4a94caf8f312ef9e74a5f26
Robt RobCas <2137000099@utna.edu.mx>
2025-03-11 16:57:11 +0000

    add middelware

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var express = require('express');
 var app = express();
 
 
-app.use("/public",express.static(__dirname +"/public"))
+app.use(express.static(__dirname +"/public"))
 
 
 app.route('/_api/package.json')
