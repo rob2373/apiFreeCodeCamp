41ff745f8cf93762f6b2b3f41ff65c603ec3de62
Robt RobCas <2137000099@utna.edu.mx>
2025-03-11 16:52:11 +0000

    add middelware

--- server.js
+++ server.js
@@ -9,20 +9,9 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 
-if (!process.env.DISABLE_XORIGIN) {
-  app.use(function(req, res, next) {
-    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
-    var origin = req.headers.origin || '*';
-    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
-         console.log(origin);
-         res.setHeader('Access-Control-Allow-Origin', origin);
-         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    }
-    next();
-  });
-}
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public",express.static(__dirname +"/public"))
+
 
 app.route('/_api/package.json')
   .get(function(req, res, next) {
